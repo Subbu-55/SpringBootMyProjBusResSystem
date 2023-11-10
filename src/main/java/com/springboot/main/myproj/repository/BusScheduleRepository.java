44be789255fddfea221b4a1a6599301ed6656dcb
Subbu-55 <138393382+Subbu-55@users.noreply.github.com>
2023-11-10 16:32:09 +0530

    final changes

--- src/main/java/com/springboot/main/myproj/repository/BusScheduleRepository.java
+++ src/main/java/com/springboot/main/myproj/repository/BusScheduleRepository.java
@@ -1,10 +1,14 @@
 package com.springboot.main.myproj.repository;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.springboot.main.myproj.model.BusSchedule;
 
 public interface BusScheduleRepository extends JpaRepository<BusSchedule, Integer>{
+
+	List<BusSchedule> findByBusOperatorId(int boid);
 
 
 }
